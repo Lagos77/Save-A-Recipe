a19e73a7e386d512a23446fb4234834ec0aeae8e
HenrikSjoe <henrik.sjogren@iths.se>
2022-02-08 13:13:49 +0100

    Initial Commit

---
+++ Save-A-Recipe/Save_A_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Save_A_RecipeApp.swift
+//  Save-A-Recipe
+//
+//  Created by Henrik Sjögren on 2022-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Save_A_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Save-A-RecipeTests/Save_A_RecipeTests.swift
@@ -0,0 +1,33 @@
+//
+//  Save_A_RecipeTests.swift
+//  Save-A-RecipeTests
+//
+//  Created by Henrik Sjögren on 2022-02-08.
+//
+
+import XCTest
+@testable import Save_A_Recipe
+
+class Save_A_RecipeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
