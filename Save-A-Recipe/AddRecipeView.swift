3c783f633ded0536024aafafda946dca90fd7f64
HenrikSjoe <henrik.sjogren@iths.se>
2022-02-14 09:59:56 +0100

    login view

--- Save-A-Recipe/AddRecipeView.swift
+++ Save-A-Recipe/AddRecipeView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import Foundation
+//import Foundation
 import Firebase
 
 struct AddRecipeView: View {
@@ -18,71 +18,159 @@ struct AddRecipeView: View {
     @State var newHowToCookSteps: [String]
     @State var recipe : Recipe?
     var db = Firestore.firestore()
+    @State var shouldShowImagePicker = false
+    @State var image: UIImage?
+    @State var loginStatusMEssage = ""
+    @State var imageRef = ""
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        
-        VStack(alignment: .center){
-            Text("Add a recipe:")//.frame(maxWidth: .infinity, alignment: .center)
-            Spacer()
-            Spacer()
-            TextField("Name your recipe", text: $newRecipeName)
-                .multilineTextAlignment(TextAlignment.center)
-                .textFieldStyle(.roundedBorder)
-            Spacer()
-            VStack(alignment: .center){
+        NavigationView {
+            ScrollView {
                 
-                HStack(alignment: .center){
+                VStack(alignment: .center){
+                    Button {
+                        shouldShowImagePicker.toggle()
+                    } label: {
+                        
+                        VStack {
+                            if let image = self.image {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .frame(width: 128, height: 128)
+                                    .cornerRadius(64)
+                                    .scaledToFill()
+                            } else {
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 64))
+                                    .padding()
+                                    .foregroundColor(Color(.label))
+                            }
+                        }
+                        .overlay(RoundedRectangle(cornerRadius: 64)
+                                    .stroke(Color.black, lineWidth: 3))
+                    }
+                    
+                    
+                    Text("Add a recipe:")//.frame(maxWidth: .infinity, alignment: .center)
                     Spacer()
-                    TextField("Add ingredient", text: $newIngredient)
+                    Spacer()
+                    TextField("Name your recipe", text: $newRecipeName)
                         .multilineTextAlignment(TextAlignment.center)
                         .textFieldStyle(.roundedBorder)
-                    
-                    Button(action: {newRecipeIngredients.append(newIngredient); newIngredient = ""}, label: {Image(systemName: "text.badge.plus")})
-                    TextField("Add step", text: $newHowToCookStep)
-                        .multilineTextAlignment(TextAlignment.center)
-                        .textFieldStyle(.roundedBorder)
-                    
-                    Button(action: {newHowToCookSteps.append(newHowToCookStep); newHowToCookStep = ""}, label: {Image(systemName: "text.badge.plus")})
                     Spacer()
+                    VStack(alignment: .center){
+                        
+                        HStack(alignment: .center){
+                            Spacer()
+                            TextField("Add ingredient", text: $newIngredient)
+                                .multilineTextAlignment(TextAlignment.center)
+                                .textFieldStyle(.roundedBorder)
+                            
+                            Button(action: {newRecipeIngredients.append(newIngredient); newIngredient = ""}, label: {Image(systemName: "text.badge.plus")})
+                            TextField("Add step", text: $newHowToCookStep)
+                                .multilineTextAlignment(TextAlignment.center)
+                                .textFieldStyle(.roundedBorder)
+                            
+                            Button(action: {newHowToCookSteps.append(newHowToCookStep); newHowToCookStep = ""}, label: {Image(systemName: "text.badge.plus")})
+                            Spacer()
+                        }
+                    }
+                    HStack{
+                        List(newRecipeIngredients, id: \.self) { recipe in
+                            Text(recipe)
+                        }.onAppear() {
+                            UITableView.appearance().backgroundColor = UIColor.clear
+                            UITableViewCell.appearance().backgroundColor = UIColor.clear
+                        }
+                        List(newHowToCookSteps, id: \.self) { step in
+                            Text(step)
+                        }
+                    }
+                    
+                    Button(action: {persistImageToStorageAndSaveRecipe(recipeName: newRecipeName, recipeIngredient: newRecipeIngredients, newHowToCook: newHowToCookSteps); presentationMode.wrappedValue.dismiss()
+                        
+                    }
+                           , label: {
+                        Text("Add to list of recipes")
+                    })
+                    //}
                 }
-            }
-            HStack{
-                List(newRecipeIngredients, id: \.self) { recipe in
-                    Text(recipe)
-                }.onAppear() {
-                    UITableView.appearance().backgroundColor = UIColor.clear
-                    UITableViewCell.appearance().backgroundColor = UIColor.clear
-                }
-                List(newHowToCookSteps, id: \.self) { step in
-                    Text(step)
-                }
-            }
-            
-            Button(action: {saveToFirestore(recipeName: newRecipeName, recipeIngredient: newRecipeIngredients, newHowToCook: newHowToCookSteps); presentationMode.wrappedValue.dismiss()
                 
             }
-                   , label: {
-                Text("Add to list of recipes")
-            })
-        }
+        }.navigationViewStyle(StackNavigationViewStyle())
+            .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+                ImagePicker(image: $image)
+            }
     }
     
-
+    private func persistImageToStorageAndSaveRecipe(recipeName: String, recipeIngredient: [String], newHowToCook : [String]) {
+   //     let fileName = UUID().uuidString
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"
+        guard let uid = Auth.auth().currentUser?.uid
+        else { return }
+       let ref = Storage.storage().reference(withPath: uid + newRecipeName)
+      //  let ref = Storage.storage().reference().child("\(uid) + \(newRecipeName).jpeg")
+       // guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        guard let imageData = (image ?? image)!.jpegData(compressionQuality: 1.0) else { return }
+        ref.putData(imageData, metadata: metadata) { metadata, err in
+            if let err = err {
+                self.loginStatusMEssage = "Failed to push image to Storage: \(err)"
+                return
+            }
+            
+           
+            
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMEssage = "Failed to retrieve downloadURL: \(err)"
+                    return
+                }
+                self.loginStatusMEssage = "Successfully stored image with imageURL \(url?.absoluteString ?? "")"
+                print(self.loginStatusMEssage)
+                imageRef = url?.absoluteString ?? ""
+                print("imageRef i persistImage = \(imageRef)")
+                print("imageRef 1 i saveToFireStore = \(imageRef)")
+//                guard let uid = Auth.auth().currentUser?.uid
+//                else { return }
+                
+                let recipe = Recipe(name: recipeName, ingredient: recipeIngredient, howToCook: newHowToCook, image: imageRef )
+                
+                do {
+                    _ = try db.collection("recipes").addDocument(from: recipe)
+                } catch {
+                    print("Error saving to DB")
+                }
+                print("imageRef 2 i saveToFireStore = \(imageRef)")
+            //    persistImageToStorage()
+                print("imageRef 3 i saveToFireStore = \(imageRef)")
+                
+                
+            }
+            
+        }
+    }
     
     func addRecipeToFB(){
         
     }
     
-    func saveToFirestore(recipeName: String, recipeIngredient: [String], newHowToCook : [String]) {
-        let recipe = Recipe(name: recipeName, ingredient: recipeIngredient, howToCook: newHowToCook, image: "https://firebasestorage.googleapis.com/v0/b/save-a-recipe2.appspot.com/o/new.jpeg?alt=media&token=af1a3dc8-6959-4d35-bd20-d4946229ebd6")
-        
-        do {
-                   _ = try db.collection("recipes").addDocument(from: recipe)
-               } catch {
-                   print("Error saving to DB")
-               }
-    }
+//    func saveToFirestore(recipeName: String, recipeIngredient: [String], newHowToCook : [String]) {
+//        print("imageRef 1 i saveToFireStore = \(imageRef)")
+//        guard let uid = Auth.auth().currentUser?.uid
+//        else { return }
+//        let recipe = Recipe(name: recipeName, ingredient: recipeIngredient, howToCook: newHowToCook, image: imageRef )
+//
+//        do {
+//            _ = try db.collection("recipes").addDocument(from: recipe)
+//        } catch {
+//            print("Error saving to DB")
+//        }
+//        print("imageRef 2 i saveToFireStore = \(imageRef)")
+//    //    persistImageToStorage()
+//        print("imageRef 3 i saveToFireStore = \(imageRef)")
+//    }
     
 }
 
@@ -93,3 +181,13 @@ struct AddRecipeView: View {
 //}
 
 
+
+/*
+rules_version = '2';
+ service firebase.storage {
+   match /b/{bucket}/o {
+     match /{allPaths=**} {
+       allow read, write: if true;
+     }
+   }
+ }*/
---
+++ Save-A-Recipe/LoginView.swift
@@ -0,0 +1,164 @@
+//
+//  LoginView.swift
+//  Save-A-Recipe
+//
+//  Created by Henrik Sjögren on 2022-02-12.
+//
+
+import SwiftUI
+import Firebase
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    @State var shouldShowImagePicker = false
+    @State var image: UIImage?
+    @State var loginStatusMEssage = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+                VStack(spacing: 16) {
+                   
+                    
+                    if !isLoginMode {
+                        Button {
+                            shouldShowImagePicker.toggle()
+                        } label: {
+                            
+                            VStack {
+                                if let image = self.image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .frame(width: 128, height: 128)
+                                        .cornerRadius(64)
+                                        .scaledToFill()
+                                } else {
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 64))
+                                        .padding()
+                                        .foregroundColor(Color(.label))
+                                }
+                            }
+                            .overlay(RoundedRectangle(cornerRadius: 64)
+                            .stroke(Color.black, lineWidth: 3))
+                        }
+                    }
+                    
+                    
+                    
+                    
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .padding(12)
+                        .background(Color.white)
+                    
+                    SecureField("Password", text: $password)
+                        .padding(12)
+                        .background(Color.white)
+                    
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight:.semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                    Text(self.loginStatusMEssage)
+                        .foregroundColor(.red)
+                }
+                .padding()
+                
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05))
+                            .ignoresSafeArea())
+        }.navigationViewStyle(StackNavigationViewStyle())
+            .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+                ImagePicker(image: $image)
+            }
+    }
+    
+   // @State var image: UIImage?
+    
+    private func handleAction() {
+        if isLoginMode {
+            loginUser()
+        } else {
+            createNewAccount()
+        }
+    }
+    
+    private func loginUser() {
+        Auth.auth().signIn(withEmail: email, password: password) {
+            result, err in
+            if let err = err {
+                print("Failed to login user", err)
+                self.loginStatusMEssage = "Failed to login user \(err)"
+                return
+            }
+            
+            print("Successfully logged in as user \(result?.user.uid ?? "")")
+            self.loginStatusMEssage = "Successfully logged in as user \(result?.user.uid ?? "")"
+        }
+    }
+    
+    
+   // @State var loginStatusMEssage = ""
+    
+    private func createNewAccount() {
+        Auth.auth().createUser(withEmail: email, password: password) {
+            result, err in
+            if let err = err {
+                print("Failed to create user", err)
+                self.loginStatusMEssage = "Failed to create user \(err)"
+                return
+            }
+            
+            print("Successfully created user \(result?.user.uid ?? "")")
+            self.loginStatusMEssage = "Successfully created user \(result?.user.uid ?? "")"
+            
+            self.persistImageToStorage()
+        }
+    }
+    
+    private func persistImageToStorage() {
+    //    let fileName = UUID().uuidString
+        guard let uid = Auth.auth().currentUser?.uid
+        else { return }
+//       let ref = Storage.storage().reference(withPath: uid)
+        let ref = Storage.storage().reference().child(uid)
+        guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
+                self.loginStatusMEssage = "Failed to push image to Storage: \(err)"
+                return
+            }
+            
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMEssage = "Failed to retrieve downloadURL: \(err)"
+                    return
+                }
+                self.loginStatusMEssage = "Successfully stored image with imageURL \(url?.absoluteString ?? "")"
+            }
+            
+        }
+    }
+    
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
