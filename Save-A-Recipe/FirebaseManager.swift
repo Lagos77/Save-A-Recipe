d6425f9032acda80ce30e3f1accc37cc8f79a012
Lagos77 <social_a@live.com>
2022-06-08 13:34:43 +0200

    ..

---
+++ Save-A-Recipe/FirebaseManager.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseManager.swift
+//  Save-A-Recipe
+//
+//  Created by Alexander on 2022-06-08.
+//
+
+import Foundation
+import FirebaseFirestore
+import Firebase
+import SwiftUI
+
+class FirebaseManager: ObservedObject{
+    
+}
